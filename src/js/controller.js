0b20067d54cae02ebba8b63739df751bd1c27070
Adnan Khan <adnanmkhan2003@gmail.com>
2023-12-02 21:14:30 +0530

    logs removed

--- src/js/controller.js
+++ src/js/controller.js
@@ -140,7 +140,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log("Welcome");
 };
 
 init();
